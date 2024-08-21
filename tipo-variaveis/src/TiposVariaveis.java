2c1f152b95e9a8b6ca96bb1faa73451bd648c6b9
Walace Nascimento <wlacens@hotmail.com>
2024-08-21 17:04:17 -0300

    exemplos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String nome = "Walace";
+        System.out.println(nome);
+
+        double salarioMinino = 2500;
+        System.out.println(salarioMinino);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        // Para declarar uma variável como uma constante, usamos a palavra "final"
+        // e o nome da variável deverá ser escrita em CAIXA ALTA
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+        
     }
 }
