0e95441d50ea21d7ed71c0c29f5e71561501bafd
Walace Nascimento <wlacens@hotmail.com>
2024-09-02 15:21:02 -0300

    Estrutura condicional - Condição ternário

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,16 +1,28 @@
 /*
 Controle de fluxo - Condicional composta 
 e Condicional encadeada
+e Condicional ternária
 */
 public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
-
+        /*  Condicional composta e encadeada
         if (nota >= 7) {
             System.out.println("Aprovado");
         } else if ( nota >= 5 && nota < 7) {
             System.out.println("Recuperação");
         } else 
             System.out.println("Reprovado");
+        */
+
+        // Condicional Ternária
+        // Cenário 1
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+
+        // Cenário 2
+		String resultado2 = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado2);
+
     }
 }
