f95fbc383347b6d769ec224b28ec13274ea6be52
Walace Nascimento <wlacens@hotmail.com>
2024-09-02 16:02:15 -0300

    Estrutura de repetição FOR

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,13 @@
+/* Estrutura de Repetição - FOR */
+public class ExemploFor {
+    public static void main(String[] args) {
+        /* Vamos imaginar que Joãozinho precisa contar até 20 
+        carneirinhos para pegar no sono:
+        */
+
+        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println(carneirinhos + " - Carneirinhos");
+        }
+        System.out.println("Joaozinho dormiu!");
+    }
+}
